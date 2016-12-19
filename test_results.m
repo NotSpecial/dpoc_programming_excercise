24d7ef2fa3ab1bec85cb99e3859bff3f46787383
NotSpecial <alexander@dietmueller.de>
2016-12-20 00:16:48 +0100

    More universal debug running script

--- test_results.m
+++ test_results.m
@@ -1,12 +1,9 @@
+% Init debug struct
+global debug;
+debug = struct();
+
 main;
 
-global timer J_debug;
-
-fprintf('Time to compute: %d s\n', timer);
-
-n_iter = size(J_debug, 2);
-
-fprintf('Iteration steps: %d\n', n_iter);
-
-figure();
-plot([1:n_iter], J_debug);
+% Reload debug struct (main clears everything) and display it
+global debug;
+disp(debug);
