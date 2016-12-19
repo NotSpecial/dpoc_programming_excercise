9b60c5a898da782b8942758ae3ee5297c5fd6ffb
NotSpecial <alexander@dietmueller.de>
2016-12-20 00:23:38 +0100

    Improved compute detection space
    Better comments
    Use probability of not being detected for most of the computations, its easier

--- ComputeDetectionSpace.m
+++ ComputeDetectionSpace.m
@@ -1,4 +1,5 @@
 function detectionSpace = ComputeDetectionSpace(stateSpace, cameras, map)
+    % Probability to be detected at each state
     n_states = size(stateSpace,1);
     detectionSpace = zeros(n_states, 1);
     for i = 1:n_states
@@ -8,20 +9,21 @@ function detectionSpace = ComputeDetectionSpace(stateSpace, cameras, map)
 end
 
 function p = detection_probability (state, cameras, map)
-    % Probability to be detected by all cameras at a certain state
+    % Distances to all cameras at given state
     distances = FreeDistances(state, cameras, map);
     
-    p = cameras(:, 3) ./ distances;
+    % Array of probabilities to NOT be detected by each camera
+    p_not = 1 - (cameras(:, 3) ./ distances);
     
     % If in pond camera can check 4 times
-    global pool_num_time_steps;
+    % Probability to not be detected is simply to the power of 4
     % Careful with map indexing! Coordinates need to be swapped.
+    global pool_num_time_steps;
     if map(state(2), state(1)) < 0
-        p = 1 - (1 - p) .^ pool_num_time_steps;
+        p_not = p_not .^ pool_num_time_steps;
     end
     
-    % Probability to be seen by any camera:
+    % Probability to be seen by any camera or several of them:
     % 1 - probabilty to be not seen by any camera
-    p = 1 - prod( (1 - p) );
-    
+    p = 1 - prod( p_not );
 end
