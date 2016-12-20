7951dbf0a7c1ff8c97f6997cdab604e8bf12592c
NotSpecial <alexander@dietmueller.de>
2016-12-20 02:34:36 +0100

    Value iteration initialized with 10. Magic.

--- ValueIteration.m
+++ ValueIteration.m
@@ -28,17 +28,19 @@ function [ J_opt, u_opt_ind ] = ValueIteration( P, G )
 %       u_opt_ind:
 %       	A (K x 1)-matrix containing the index of the optimal control
 %       	input for each element of the state space.
-global timer;
-tic;
+
+% Debugging
+global debug;
+start = tic;
+
 % Initialize J_opt and u_opt_ind
-% Use approximate map size as initialization (WIP)
+% Initializing J_opt with 10 seems to work really well in most situations
 K = size(P, 1);
-J_opt = sqrt(K) * ones(K, 1);
+J_opt = 10 * ones(K, 1);
 u_opt_ind = zeros(K, 1);
 tolerance = 1e-5;
 
 % Debugging
-global J_debug
 J_debug = J_opt;
 
 while 1
@@ -57,5 +59,6 @@ while 1
     end
 end
 
-timer = toc;
+debug.time_valueit = toc(start);
+debug.J_val = J_debug;
 end
